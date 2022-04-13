9ad21bb08e3f8b0f32ccf7c5f0ddf863ad06dbdf
badgley <graysonbadgley@gmail.com>
2022-04-13 21:16:52 +0000

    Add tanoak proximity analysis; reorganize figure generation

---
+++ buffer_integrity/analysis/buffer_pool_composition.py
@@ -0,0 +1,134 @@
+import json
+from pathlib import Path
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from carbonplan_styles.colors import light
+from carbonplan_styles.mpl import set_theme
+
+set_theme(font_scale=1.25)
+mpl.rc("font", **{"family": "sans-serif", "sans-serif": ["Helvetica"]})
+
+
+def main():
+    with open(Path(__file__).parents[2] / "data" / "buffer_contributions.json") as f:
+        d = json.load(f)
+    natural_risk_buffer = sum([v for k, v in d.items() if not k.startswith("gross")])
+
+    non_natural = d["gross_buffer"] - natural_risk_buffer
+
+    height = 0.45
+    fig, ax = plt.subplots()
+    fig.set_size_inches(7, 3.5)
+    n_bars = 5
+
+    breaks = np.linspace(0, n_bars * height, 5)
+
+    ax.barh(
+        breaks[-1],
+        d["gross_buffer"],
+        height=height,
+        color=light["secondary"],
+        edgecolor="None",
+    )
+    ax.barh(
+        breaks[-2],
+        d["fire_contributions"],
+        height=height,
+        color=light["red"],
+        edgecolor="None",
+    )
+    ax.barh(
+        breaks[-3],
+        d["pest_contributions"],
+        height=height,
+        left=d["fire_contributions"],
+        color=light["blue"],
+        edgecolor="None",
+    )
+    ax.barh(
+        breaks[-4],
+        d["other_contributions"],
+        height=height,
+        left=d["fire_contributions"] + d["pest_contributions"],
+        color=light["orange"],
+        edgecolor="None",
+    )
+    ax.barh(
+        breaks[-5],
+        d["gross_buffer"] - natural_risk_buffer,
+        height=height,
+        left=natural_risk_buffer,
+        color=light["purple"],
+        edgecolor="None",
+    )
+
+    ax.annotate(text="Total Buffer Pool", xy=(0, 0.98), xycoords="axes fraction")
+    ax.annotate(
+        text=f"{d['gross_buffer'] / 1_000_000:.1f}$\,$M ({d['gross_buffer']/ d['gross_buffer'] * 100:.0f}%)",  # noqa
+        xy=(0.35, 0.98),
+        color=light["secondary"],
+        xycoords="axes fraction",
+    )
+
+    ax.annotate(text="Wildfire", xy=(0.22, 0.65), xycoords="axes fraction")
+    ax.annotate(
+        text=f"{d['fire_contributions'] / 1_000_000:.1f}$\,$M ({d['fire_contributions']/ d['gross_buffer'] * 100:.0f}%)",  # noqa
+        xy=(0.385, 0.65),
+        color=light["red"],
+        xycoords="axes fraction",
+    )
+
+    ax.annotate(text="Disease & Insect", xy=(0.41, 0.47), xycoords="axes fraction")
+    ax.annotate(
+        text=f"{d['pest_contributions'] / 1_000_000:.1f}$\,$M ({d['pest_contributions']/ d['gross_buffer'] * 100:.0f}%)",  # noqa
+        xy=(0.75, 0.47),
+        color=light["blue"],
+        xycoords="axes fraction",
+    )
+
+    ax.annotate(text="Other", xy=(0.57, 0.29), xycoords="axes fraction")
+    ax.annotate(
+        text=f"{d['other_contributions'] / 1_000_000:.1f}$\,$M ({d['other_contributions']/ d['gross_buffer'] * 100:.0f}%)",  # noqa
+        xy=(0.70, 0.29),
+        color=light["orange"],
+        xycoords="axes fraction",
+    )
+
+    ax.annotate(text="Financial & Management", xy=(0.565, -0.05), xycoords="axes fraction")
+    ax.annotate(
+        text=f"{ non_natural / 1_000_000:.1f}$\,$M ({non_natural/ d['gross_buffer'] * 100:.0f}%)",  # noqa
+        xy=(1.065, -0.05),
+        color=light["purple"],
+        xycoords="axes fraction",
+    )
+
+    plt.ylim(0 - height, breaks[-1] + height)
+    plt.xlim(0, d["gross_buffer"])
+
+    plt.axis("off")
+
+    ax.annotate(
+        text="Natural\nRisks",
+        xy=(-0.2, 0.4),
+        xycoords="axes fraction",
+        horizontalalignment="center",
+    )
+    ax.annotate(
+        text="Non-natural\nRisks",
+        xy=(-0.2, 0),
+        xycoords="axes fraction",
+        horizontalalignment="center",
+    )
+
+    # ax.vlines(x=-10, ymin=0, ymax=1)
+    plt.savefig(
+        Path(__file__).parents[2] / "img" / "buffer_pool_composition.pdf",
+        dpi=300,
+        bbox_inches="tight",
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ buffer_integrity/analysis/tanoak_proximity.py
@@ -0,0 +1,38 @@
+import json
+from pathlib import Path
+
+import fsspec
+import tqdm
+from shapely.ops import nearest_points
+
+from buffer_integrity import utils
+
+
+def get_nearest_point(geometry, points):
+
+    points = nearest_points(geometry, points)
+    distance = points[0].distance(points[1])  # / 1_000 # km
+    return distance
+
+
+def main():
+    with open(Path(__file__).parents[1] / "data" / "tanoak_basal_area.json") as f:
+        tanoak_projects = json.load(f)
+
+    sod_blitz = utils.load_sod_blitz()
+    positive_sod_blitz = sod_blitz[sod_blitz["is_positive"]].to_crs("epsg:5070").unary_union
+
+    distances = {}
+
+    for opr_id in tqdm.tqdm(tanoak_projects.keys()):
+        gdf = utils.load_project_geometry(opr_id).to_crs("epsg:5070")
+        geometry = gdf.iloc[0].geometry
+        distance = get_nearest_point(geometry, positive_sod_blitz) / 1_000  # km
+        distances[opr_id] = distance
+
+    with fsspec.open("gs://carbonplan-buffer-analysis/tanoak/distance-to-sod-blitz.json", "w") as f:
+        json.dump(distances, f)
+
+
+if __name__ == "__main__":
+    main()
