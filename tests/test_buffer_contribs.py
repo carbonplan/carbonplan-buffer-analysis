6b1c1e7a1a4795c014ef4aa0e625687c7231f2eb
badgley <graysonbadgley@gmail.com>
2022-04-14 21:06:52 +0000

    Add tests to repo

---
+++ tests/test_buffer_contribs.py
@@ -0,0 +1,9 @@
+from buffer_integrity.prefect.flows import calculate_buffer_contributions
+
+
+def test_all_projects():
+    issuance = calculate_buffer_contributions.get_issuance_table.run()
+    forest_projects = issuance.loc[issuance['project_type'] == 'forest', 'opr_id'].unique().tolist()
+    fire_risks = calculate_buffer_contributions.load_project_fire_risks.run()
+    for forest_project in forest_projects:
+        assert forest_project in fire_risks
---
+++ tests/test_tanoak_loss.py
@@ -0,0 +1,80 @@
+import pytest
+
+from buffer_integrity.prefect.flows.calculate_tanoak_basal_area import get_fraction_tanoak
+
+
+@pytest.mark.parametrize(
+    "project, expected_result",
+    [
+        (
+            {
+                "opr_id": "XYZ123",
+                "acreage": 100,
+                "assessment_areas": [
+                    {
+                        "code": 1,
+                        "site_class": "low",
+                        "acreage": 100,
+                        "species": [{"code": 999, "fraction": 1}],
+                    }
+                ],
+            },
+            0,
+        ),
+        (
+            {
+                "opr_id": "XYZ321",
+                "acreage": 100,
+                "assessment_areas": [
+                    {
+                        "code": 1,
+                        "site_class": "low",
+                        "acreage": 100,
+                        "species": [
+                            {"code": 631, "fraction": 0.5},
+                            {"code": 999, "fraction": 0.5},
+                        ],
+                    }
+                ],
+            },
+            0.5,
+        ),
+        (
+            {
+                "opr_id": "XYZ007",
+                "acreage": 100,
+                "assessment_areas": [
+                    {
+                        "code": 1,
+                        "site_class": "low",
+                        "acreage": 50,
+                        "species": [
+                            {"code": 631, "fraction": 0.5},
+                            {"code": 999, "fraction": 0.5},
+                        ],
+                    },
+                    {
+                        "code": 1,
+                        "site_class": "high",
+                        "acreage": 50,
+                        "species": [
+                            {"code": 631, "fraction": 0.5},
+                            {"code": 999, "fraction": 0.5},
+                        ],
+                    },
+                ],
+            },
+            0.5,
+        ),
+        (
+            {
+                "opr_id": "XYZ999",
+                "acreage": 100,
+                "assessment_areas": [],
+            },
+            0,
+        ),
+    ],
+)
+def test_get_tanaok_fraction(project, expected_result):
+    assert get_fraction_tanoak(project) == expected_result
